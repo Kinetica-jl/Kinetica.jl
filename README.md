fa1ae858abd4c0bd4a493d6d308548d86fc7044d
joegilkes <j.gilkes@warwick.ac.uk>
2023-08-23 16:44:09 +0100

    Added automatic CDE compilation to package build

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # KineticaCore
+
+## Installation
+
+When installing *KineticaCore* from Julia's Package Registry, a local binary of CDE will need to be compiled. CDE is included as a submodule in *KineticaCore*'s GitHub reposity, and can be automatically compiled in the correct location when building the package. As such, any environment variables that are required for CDE compilation should be set before instantiating the Julia REPL where *KineticaCore* is installed/built.
+
+For example, if CDE is to be compiled with `ifort`, this should be set within the environment:
+
+```bash
+$ FC=ifort julia
+julia> using Pkg
+julia> Pkg.add("KineticaCore")
+```
+
+This will pass the Fortran compiler through to *KineticaCore*'s build file `deps/build.jl`, which will compile CDE.
+
+In case of any errors, full details are located in the build log at `deps/build.log`. While it should cover most setups, CDE's `Makefile` may need to be modified for compilation to succeed. After a failed build (which instantiated the git submodule for CDE), the `Makefile` can be accessed from `submodules/cde/Makefile`. After any edits have been made, *KineticaCore* can be rebuilt from the REPL to recompile CDE:
+
+```bash
+$ FC=ifort julia
+julia> using Pkg
+julia> Pkg.build("KineticaCore")
+```
--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,41 @@
-println("I am being built...")
+PROJECT_DIR = dirname(dirname(@__FILE__))
+
+# Check git is installed.
+gitcheck = run(Cmd(`which git`, ignorestatus=true))
+if gitcheck.exitcode == 1
+    error("Git not found on the system. Install Git before running this script.")
+end
+
+# Get dependencies
+depsdir = joinpath(PROJECT_DIR, "submodules")
+cdedir = joinpath(depsdir, "cde")
+if readdir(cdedir) != String[]
+    @info string("Submodules already initialised, checking for updates...")
+    gitcmd = run(Cmd(`git submodule update`, dir=PROJECT_DIR))
+    if gitcmd.exitcode == 1
+        error("Error updating Git submodules.")
+    end
+    @info string("Submodules up to date.")
+else
+    @info string("Obtaining and initialising submodules...")
+    gitcmd = run(Cmd(`git submodule init`, dir=PROJECT_DIR))
+    if gitcmd.exitcode == 1
+        error("Error initialising Git submodules.")
+    end
+    gitcmd = run(Cmd(`git submodule update --init`, dir=PROJECT_DIR))
+    if gitcmd.exitcode == 1
+        error("Error initialising Git submodules.")
+    end
+    @info string("All submodules initialised.")
+end
+
+# Compile CDE.
+@info string("CDE may require some configuration before compilation.")
+@info string("Compiling CDE...")
+cdemakecmd = run(Cmd(`make`, dir=cdedir))
+if cdemakecmd.exitcode == 1
+    error("CDE failed to compile!")
+end
+
+@info string("CDE compilation complete.")
+@info string("KineticaCore setup complete.")
