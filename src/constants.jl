5ec0c0d4ed4c4a511ca6541f12e9e55c6d220951
joegilkes <j.gilkes@warwick.ac.uk>
2023-09-27 10:54:55 +0100

    Added new conversion factors

--- src/constants.jl
+++ src/constants.jl
@@ -9,5 +9,7 @@ const h = 6.626070e-34 # Planck constant (Js)
 # Conversion Factors
 const kcal_to_J = 4183.9954
 const eV_to_kcal_per_mol = 23.061
+const amu_to_kg = 1.6605390666e-27
+const Ang_to_dm = 1e-9
 
 end
