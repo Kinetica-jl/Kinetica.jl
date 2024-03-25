939ac489b849d76de14a1fd0afc1345bc74856bb
joegilkes <j.gilkes@warwick.ac.uk>
2024-03-25 11:36:09 +0000

    Bump to v0.5.1

--- src/Kinetica.jl
+++ src/Kinetica.jl
@@ -26,7 +26,7 @@ using OrderedCollections
 using PythonCall
 using CDE_jll
 
-const version = VersionNumber(0, 5, 0)
+const version = VersionNumber(0, 5, 1)
 
 # Global Python package interfaces
 const pybel = PythonCall.pynew()
