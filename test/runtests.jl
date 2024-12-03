2abb6ca05e3f7188a07d3cec759a8e6e76f54c0e
Joe Gilkes <j.gilkes@warwick.ac.uk>
2024-12-03 10:37:05 +0000

    Removed unit test grouping

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,28 +4,25 @@ using SafeTestsets
 using Random
 Random.seed!(12345)
 
-const GROUP = get(ENV, "GROUP", "All")
-
-if GROUP == "All" || GROUP == "Main"
-    @safetestset "ConditionSet Tests" begin
-        include("Main/conditions.jl")
-    end
+# Python tests - should come first, as they are most likely to have issues and
+# many other bits depend on them down the chain.
+@safetestset "OpenBabel Tests" begin
+    include("Python/openbabel.jl")
+end
+@safetestset "RDKit Tests" begin
+    include("Python/rdkit.jl")
+end
+@safetestset "ASE Tests" begin
+    include("Python/ase.jl")
+end
+@safetestset "Surface Tests" begin
+    include("Python/surfaces.jl")
+end
+@safetestset "autodE Tests" begin
+    include("Python/autode.jl")
 end
 
-if GROUP == "All" || GROUP == "Python"
-    @safetestset "OpenBabel Tests" begin
-        include("Python/openbabel.jl")
-    end
-    @safetestset "RDKit Tests" begin
-        include("Python/rdkit.jl")
-    end
-    @safetestset "ASE Tests" begin
-        include("Python/ase.jl")
-    end
-    @safetestset "Surface Tests" begin
-        include("Python/surfaces.jl")
-    end
-    @safetestset "autodE Tests" begin
-        include("Python/autode.jl")
-    end
+# Pure Julia tests.
+@safetestset "ConditionSet Tests" begin
+    include("Main/conditions.jl")
 end
